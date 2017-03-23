13f3f3230c256587d0e67ef4995195c30f742055
yanyuan <yuan.yan@dianrong.com>
2017-03-23 18:31:24 +0800

    task8 second

--- IFE2017/task8/app.js
+++ IFE2017/task8/app.js
@@ -1,22 +1,69 @@
 var myChart = echarts.init(document.getElementById('container'));
 
+var xData = function() {
+  var data = [];
+  for(var i=1; i < 15; i++) {
+    data.push(i+"");
+  }
+  return data;
+}();
+
 var option = {
+  backgroundColor: '#344b58',
   title: {
-    text: 'ECharts入门'
+    text: 'ECharts入门',
+    subtext: 'By YanYuanFE',
+    x: '4%',
+    textStyle: {
+      color:'#fff',
+      fontSize:'22'
+    },
+    subtextStyle: {
+      color:'#90979c',
+      fontSize:'16',
+    },
   },
-  tooltip: {},
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
+      textStyle: {
+        color: '#fff'
+      }
+    }
+  },
+  grid: {
+    borderWidth: 0,
+    top: 110,
+    bottom: 95,
+    textStyle: {
+      color: '#fff'
+    }
+  },
   legend: {
-    data: ['销量']
+    x: '4%',
+    top: '11%',
+    textStyle: {
+      color: '#90979c',
+    },
+    data: ['空气温度','空气湿度','土壤湿度']
   },
   xAxis: {
       data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
   },
   yAxis: {},
-  series: [{
+  series: [
+    {
       name: '销量',
       type: 'bar',
       data: [5, 20, 36, 10, 10, 20]
-  }]
+    },
+    {
+      name: '销量',
+      type: 'line',
+      data: [5, 20, 36, 10, 10, 20]
+    },
+  ]
 };
 
 myChart.setOption(option);
